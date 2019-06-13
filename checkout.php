2381ba4ec8acf0e00aba64f6c7c3c02c406e8d24
AlejoChol <37298282+AlejoChol@users.noreply.github.com>
2019-06-13 10:47:11 -0300

    Agregado comunicacion por metodo get

--- checkout.php
+++ checkout.php
@@ -2,14 +2,15 @@
 <body>
 <?php
 
-//Conexion a base de datos
+//Conexion a base de datos - CAMBIAR CREDENCIALES DE LOGUEO
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
 $dbname = "restaurant";
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);  
 
-	$Categoria=($_POST['category']);
+	$Busqueda=($_POST['search_value']);
+	$Categoria=($_POST['search_category']);
 	$result = mysqli_query($conn, "SELECT * WHERE '$Categoria' LIKE %'$Busqueda'%");
 	echo json_encode($result); exit();
 ?>
